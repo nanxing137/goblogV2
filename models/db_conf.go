4b7dbca2e6291e8f6b74de0ad21f54c7e9023789
gaopeng09 <gaopeng09@baidu.com>
2019-09-25 17:13:39 +0800

    format

---
+++ models/db_conf.go
@@ -0,0 +1,51 @@
+package models
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	setting "github.com/thornshell/goblogV2/pkg"
+	"log"
+)
+
+var Db *gorm.DB
+
+func init() {
+	var (
+		err                                               error
+		dbType, dbName, user, password, host, tablePrefix string
+	)
+
+	sec, err := setting.Cfg.GetSection("database")
+	if err != nil {
+		log.Fatal(2, "Fail to get section 'database': %v", err)
+	}
+
+	dbType = sec.Key("TYPE").String()
+	dbName = sec.Key("NAME").String()
+	user = sec.Key("USER").String()
+	password = sec.Key("PASSWORD").String()
+	host = sec.Key("HOST").String()
+	tablePrefix = sec.Key("TABLE_PREFIX").String()
+
+	Db, err = gorm.Open(dbType, fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
+		user,
+		password,
+		host,
+		dbName))
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	gorm.DefaultTableNameHandler = func(db *gorm.DB, defaultTableName string) string {
+		return tablePrefix + defaultTableName
+	}
+
+	Db.SingularTable(true)
+	Db.DB().SetMaxIdleConns(10)
+	Db.DB().SetMaxOpenConns(100)
+}
+
+func CLoseDB() {
+	defer Db.Close()
+}
